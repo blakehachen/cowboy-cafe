a95dcd9d2801172091f9c7738357dc411d93fcce
Blake Hachen <blakehachen@users.campus>
2020-02-26 17:13:10 -0600

    Added Order Summary Control

---
+++ PointOfSale/OrderSummaryControl.xaml.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using CowboyCafe.Data;
+
+namespace PointOfSale
+{
+    /// <summary>
+    /// Interaction logic for OrderSummaryControl.xaml
+    /// </summary>
+    public partial class OrderSummaryControl : UserControl
+    {
+        public OrderSummaryControl()
+        {
+            InitializeComponent();
+        }
+
+
+        /// <summary>
+        /// Adds Cowpoke Chili to the order list
+        /// </summary>
+        private void AddCowpokeChiliButton_Click(object sender, RoutedEventArgs e)
+        {
+            OrderList.Items.Add(new CowpokeChili());
+        }
+
+        /// <summary>
+        /// Adds Rustlers Ribs to the order list
+        /// </summary>
+        private void AddRustlersRibsButton_Click(object sender, RoutedEventArgs e)
+        {
+            OrderList.Items.Add(new RustlersRibs());
+        }
+
+        /// <summary>
+        /// Adds Pecos Pulled Pork to the order list
+        /// </summary>
+        private void AddPecosPulledPorkButton_Click(object sender, RoutedEventArgs e)
+        {
+            OrderList.Items.Add(new PecosPulledPork());
+        }
+
+        /// <summary>
+        /// Add Angry Chicken to the order list
+        /// </summary>
+        private void AddAngryChickenButton_Click(object sender, RoutedEventArgs e)
+        {
+            OrderList.Items.Add(new AngryChicken());
+        }
+
+        /// <summary>
+        /// Add Trail Burger to the order list
+        /// </summary>
+        private void AddTrailBurgerButton_Click(object sender, RoutedEventArgs e)
+        {
+            OrderList.Items.Add(new TrailBurger());
+        }
+
+        /// <summary>
+        /// Add Dakota Double Burger to the order list
+        /// </summary>
+        private void AddDakotaDoubleBurgerButton_Click(object sender, RoutedEventArgs e)
+        {
+            OrderList.Items.Add(new DakotaDoubleBurger());
+        }
+
+        /// <summary>
+        /// Add Texas Triple Burger to the order list
+        /// </summary>
+        private void AddTexasTripleBurgerButton_Click(object sender, RoutedEventArgs e)
+        {
+            OrderList.Items.Add(new TexasTripleBurger());
+        }
+
+        /// <summary>
+        /// Add Chili Cheese Fries to the order list
+        /// </summary>
+        private void AddChiliCheeseFriesButton_Click(object sender, RoutedEventArgs e)
+        {
+            OrderList.Items.Add(new ChiliCheeseFries());
+        }
+
+        /// <summary>
+        /// Add Corn Dodgers to the order list
+        /// </summary>
+        private void AddCornDodgersButton_Click(object sender, RoutedEventArgs e)
+        {
+            OrderList.Items.Add(new CornDodgers());
+        }
+
+        /// <summary>
+        /// Add Pan De Campo to the order list
+        /// </summary>
+        private void AddPanDeCampoButton_Click(object sender, RoutedEventArgs e)
+        {
+            OrderList.Items.Add(new PanDeCampo());
+        }
+
+        /// <summary>
+        /// Add Baked Beans to the order list
+        /// </summary>
+        private void AddBakedBeansButton_Click(object sender, RoutedEventArgs e)
+        {
+            OrderList.Items.Add(new BakedBeans());
+        }
+
+        /// <summary>
+        /// Add Jerked Soda to the order list
+        /// </summary>
+        private void AddJerkedSodaButton_Click(object sender, RoutedEventArgs e)
+        {
+            OrderList.Items.Add(new JerkedSoda());
+        }
+
+        /// <summary>
+        /// Add Texas Tea to the order list
+        /// </summary>
+        private void AddTexasTeaButton_Click(object sender, RoutedEventArgs e)
+        {
+            OrderList.Items.Add(new TexasTea());
+        }
+
+        /// <summary>
+        /// Add Cowboy Coffee to the order list
+        /// </summary>
+        private void AddCowboyCoffeeButton_Click(object sender, RoutedEventArgs e)
+        {
+            OrderList.Items.Add(new CowboyCoffee());
+        }
+
+        /// <summary>
+        /// Add Wate to the order list
+        /// </summary>
+        private void AddWaterButton_Click(object sender, RoutedEventArgs e)
+        {
+            OrderList.Items.Add(new Water());
+        }
+    }
+}
